03fe6c8dbf5b3392fc5f8221c594cb2faebc0473
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-06-01 15:35:27 -0300

    Requisito 7 concluido, implementação funções calculator e arrayGenerator

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,22 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => ({ sum: number1 + number2, 
+  mult: number1 * number2,
+  div: Math.floor(number1 / number2),
+  sub: number1 - number2 });
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  switch (type) {
+    case 'keys':
+      return Object.keys(object);
+    case 'values':
+      return Object.values(object);
+    case 'entries':
+      return Object.entries(object);
+    default:
+      break;
+  }
+};
 
 module.exports = { calculator, arrayGenerator };
