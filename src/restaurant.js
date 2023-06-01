8f007292079dcd5a155298e2d9a7101115f93b52
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-06-01 17:50:57 -0300

    Requisito 10 concluido, implementação função restaurant e tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,20 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
+const createMenu = (object) => {
+ const obj = { 
+        fetchMenu: () => object, 
+        consumption: [], 
+        order: (prod) => { 
+          if (!object.food[prod] && !object.drinks[prod]) {
+            throw new Error('Item indisponível');
+          } else {
+            obj.consumption.push(prod);
+          }
+        },
+      };
+  return obj;
+};
 
+// console.log(createMenu({}));
 module.exports = createMenu;
